eec2cfa0e75f4505796923a9a3c94bd159546017
Cam Crews <camcrews@gmail.com>
2017-06-12 17:44:19 -0400

    A Jukebox for Simpler Times

--- juke-soundcloud3/js/main.js
+++ juke-soundcloud3/js/main.js
@@ -12,6 +12,7 @@ function Jukebox(){
   this.players = []; // the SoundCloud player object
   this.currentSong = 0;
   this.SC = SC;
+  this.isRandom = false;
   this.SC.initialize({
     client_id: 'fd4e76fc67798bfa742089ed619084a6'
   });
@@ -23,54 +24,101 @@ Jukebox.prototype.addSong = function(){
     this.songs.push( arguments[i] );
   }
 }
+// change the song and update the player
+Jukebox.prototype.changeSong = function(i=0){
+  if( this.isRandom ) {
+    this.currentSong = Math.floor( Math.random() * this.songs.length );
+  } else {
+    this.currentSong = Math.abs(this.currentSong + this.songs.length + i) % this.songs.length;
+  }
+  // update player and song reference (for convenience)
+  this.player = this.players[this.currentSong];
+  this.song = this.songs[this.currentSong]
+}
+
 // play the player for the current track
 Jukebox.prototype.play = function(){
-  console.log( "in play", this );
   const self = this;
-  let player = this.players[this.currentSong],
-      song = this.songs[this.currentSong];
+
+  // moved the following to changeSong()
+  // let player = this.players[this.currentSong],
+  //     song = this.songs[this.currentSong];
+
   // see if we already have a player for
   // the current song...  if so, use that
-  if( player ) {
-    console.log( "player detected" );
-    player.play();
-    // to do: update the player song info
+  if( this.players[this.currentSong] ) {
+    this.changeSong();
+    this.players[this.currentSong].play();
+    // update the player song info
+    document.querySelector('.track').innerHTML = this.songs[this.currentSong].title;
   } else {
-    console.log( "no player detected" );
-  // else, go and fetch a player, then play
-    this.SC.stream('/tracks/'+song.id).then(function(p){
-      console.log( "got player", p);
-      self.players[self.currentSong] = p;
-      console.log( self.players );
-      self.play();
+    let track_id = self.currentSong;
+    // else, go and fetch a player, then play
+    this.SC.stream('/tracks/'+self.songs[self.currentSong].id).then(function(player){
+        self.players[track_id] = player;
+        self.play();
     });
   }
 }
 // pause the player for the current track
 Jukebox.prototype.pause = function(){
-  // to do: implement
+  if( this.player ) {
+    this.player.pause();
+  }
 }
 // stop the player for current track
 Jukebox.prototype.stop = function(){
-  // to do: ...
+  if( this.player ) {
+    this.player.pause();
+    this.player.seek(0);
+  }
 }
 // advance to next song
 Jukebox.prototype.next = function(){
-  // do too: ...
+  this.changeSong(1);
+  this.play();
 }
 // go back one song
 Jukebox.prototype.back = function(){
-  // to do: ..
+  this.changeSong(-1);
+  this.play();
 }
-
-var myJukebox = new Jukebox();
-myJukebox.addSong(new Song('45062705', 'Hotel Dennis'), new Song('223228432',"It's strange"), new Song('29366957',"Mac Miller Heat Wave"));
+// toggle random flag
+Jukebox.prototype.random = function(){
+  this.isRandom = !this.isRandom;
+}
+var myJukebox;
 
 document.addEventListener("DOMContentLoaded",function(){
-  document.querySelector('.play').addEventListener('click',function(event){
-    myJukebox.play();
+  myJukebox = new Jukebox();
+  myJukebox.addSong(new Song('45062705', 'Hotel Dennis'), new Song('223228432',"It's strange"), new Song('29366957',"Mac Miller Heat Wave"));
+  
+  // the lines above replace the longer block of commented out code
+  ['play','pause','back','stop','next','random'].forEach( action => {
+    console.log( action );
+    document.querySelector('.'+action).addEventListener('click',function(event){
+      console.log( myJukebox[action] );
+      myJukebox[action].call(myJukebox);
+    });
   });
-});
 
-// "It's strange"
-// 'It\' strange'
+  // document.querySelector('.play').addEventListener('click',function(event){
+  //   myJukebox.play();
+  // });
+  // document.querySelector('.next').addEventListener('click',function(event){
+  //   myJukebox.next();
+  // });
+  // document.querySelector('.pause').addEventListener('click',function(event){
+  //   myJukebox.pause();
+  // });
+  // document.querySelector('.stop').addEventListener('click',function(event){
+  //   myJukebox.stop();
+  // });
+  // document.querySelector('.back').addEventListener('click',function(event){
+  //   myJukebox.back();
+  // });
+  // document.querySelector('.random').addEventListener('click',function(event){
+  //   myJukebox.random();
+  // });
+
+});

713f594b18927b91cdd8d53b0236e6fea3b12187
Cam Crews <camcrews@gmail.com>
2017-06-26 16:16:06 -0400

    Sendgrid API integration

--- codewars/app.rb
+++ codewars/app.rb
@@ -1,9 +1,37 @@
 require 'sinatra'
+# using SendGrid's Ruby Library
+# https://github.com/sendgrid/sendgrid-ruby
+require 'sendgrid-ruby'
+include SendGrid
 
 get '/' do
   @roster = %w(Julia Logan Nick Katy Dennis Neil Hans James Victor Steven Erin)
   erb :home
 end
+
+get '/contact' do
+  erb :contact
+end
+
+post '/contact' do
+  # get from address from our form
+  # to do: verify the email is valid
+  from = Email.new(email: params[:email])
+  to = Email.new(email: 'cam@nycda.com')
+  # get subject from our form
+  subject = params[:title]
+  # get content from our form
+  content = Content.new(type: 'text/plain', value: params[:body])
+  mail = Mail.new(from, subject, to, content)
+
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  puts response.status_code
+  puts response.body
+  puts response.headers
+  redirect '/contact'
+end
+
 
 get '/profile/:person' do
   @roster = {

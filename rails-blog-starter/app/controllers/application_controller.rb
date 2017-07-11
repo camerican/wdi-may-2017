4d254426482a802f8b6e75a940a671900697a784
Cam Crews <camcrews@gmail.com>
2017-07-11 14:52:08 -0400

    @current_user

--- rails-blog-starter/app/controllers/application_controller.rb
+++ rails-blog-starter/app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :current_user
+
+  private
+
+  def current_user
+    #@current_user = session[:user_id] ? User.find(session[:user_id]) : nil
+    begin
+      @current_user = session[:user_id] && User.find(session[:user_id])
+    rescue
+      puts "session[:user_id] error (#{session[:user_id]})"
+      session[:user_id] = nil
+    end
+  end
+
 end
